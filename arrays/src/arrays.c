71517d54f2a2dec6bc85e69fb2b652c10a46f8c2
Skinner <skinnerh@tc-login.cobalt.lan>
2017-01-21 13:00:06 -0600

    Finished array_copy

--- arrays/src/arrays.c
+++ arrays/src/arrays.c
@@ -5,29 +5,38 @@
 #include "../include/arrays.h"
 
 // LOOK INTO MEMCPY, MEMCMP, FREAD, and FWRITE
-
+// Copy the contents from source array into destination array
+// \param src the array the will be copied into the destination array
+// \param dst the array that will be copied from the source array
+// \param elem_size the number of bytes each array element uses
+// \param elem_count the number of elements in the source array
+// return true if operation was successful, else false
 bool array_copy(const void *src, void *dst, const size_t elem_size, const size_t elem_count) {
-    int *test = malloc(5);
-    free(test);
-	return false;
+    if (!src || !dst || elem_size == 0 || elem_count == 0) {
+        return false;
+    }
+    if (!memcpy(dst, src, (elem_size * elem_count))) {
+        return false;
+    }
+    return true;
 }
 
 bool array_is_equal(const void *data_one, void *data_two, const size_t elem_size, const size_t elem_count) {
 
-	return false;
+    return false;
 }
 
 ssize_t array_locate(const void *data, const void *target, const size_t elem_size, const size_t elem_count) {
 
-	return 0;
+    return 0;
 }
 
 bool array_serialize(const void *src_data, const char *dst_file, const size_t elem_size, const size_t elem_count) {
 
-	return false;
+    return false;
 }
 
 bool array_deserialize(const char *src_file, void *dst_data, const size_t elem_size, const size_t elem_count) {
 
-	return false;
+    return false;
 }
