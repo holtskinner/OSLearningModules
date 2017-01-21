cf500abc635cd8141f4cde7874ab3052cd3523c4
Holt Skinner <HoltWasHere@gmail.com>
2017-01-21 13:41:07 -0600

    COded array_serialize

--- arrays/src/arrays.c
+++ arrays/src/arrays.c
@@ -5,46 +5,67 @@
 #include "../include/arrays.h"
 
 // LOOK INTO MEMCPY, MEMCMP, FREAD, and FWRITE
-// Copy the contents from source array into destination array
-// \param src the array the will be copied into the destination array
-// \param dst the array that will be copied from the source array
-// \param elem_size the number of bytes each array element uses
-// \param elem_count the number of elements in the source array
-// return true if operation was successful, else false
-bool array_copy(const void *src, void *dst, const size_t elem_size, const size_t elem_count) {
-    if (!src || !dst || elem_size == 0 || elem_count == 0) {
-        return false;
-    }
-    if (!memcpy(dst, src, (elem_size * elem_count))) {
-        return false;
-    }
+
+bool array_copy(const void *src, void *dst, const size_t elem_size,
+                const size_t elem_count) {
+  // sanity check
+  if (!src || !dst || elem_size == 0 || elem_count == 0) {
+    return false;
+  }
+  // copy memory and check for NULL
+  if (!memcpy(dst, src, (elem_size * elem_count))) {
+    return false;
+  }
+  return true;
+}
+
+bool array_is_equal(const void *data_one, void *data_two,
+                    const size_t elem_size, const size_t elem_count) {
+  // sanity check
+  if (!data_one || !data_two || elem_size == 0 || elem_count == 0) {
+    return false;
+  }
+
+  // memcmp returns 0 if memory is equal
+  if (memcmp(data_one, data_two, (elem_size * elem_count)) == 0) {
     return true;
+  }
+
+  return false;
 }
 
-bool array_is_equal(const void *data_one, void *data_two, const size_t elem_size, const size_t elem_count) {
-    if (!data_one || !data_two || elem_size == 0 || elem_count == 0) {
-        return false;
-    }
-
-    int result = memcmp(data_one, data_two, (elem_size * elem_count));
-
-    if (result == 0) {
-        return true;
-    }
+bool array_serialize(const void *src_data, const char *dst_file,
+                     const size_t elem_size, const size_t elem_count) {
+  // sanity check
+  if (!src_data || !dst_file || elem_size == 0 || elem_count == 0) {
     return false;
-}
+  }
 
-ssize_t array_locate(const void *data, const void *target, const size_t elem_size, const size_t elem_count) {
+  // open file for writing in binary
+  FILE *fp = fopen(dst_file, "wb");
 
-    return 0;
-}
-
-bool array_serialize(const void *src_data, const char *dst_file, const size_t elem_size, const size_t elem_count) {
-
+  if (!fp) {
+    fclose(fp);
     return false;
+  }
+
+  // fwrite returns the number of elements written (should be the same as
+  // elem_count)
+  if (elem_count != fwrite(src_data, elem_size, elem_count, fp)) {
+    fclose(fp);
+    return false;
+  }
+
+  fclose(fp);
+  return true;
 }
 
-bool array_deserialize(const char *src_file, void *dst_data, const size_t elem_size, const size_t elem_count) {
+bool array_deserialize(const char *src_file, void *dst_data,
+                       const size_t elem_size, const size_t elem_count) {
+  return false;
+}
 
-    return false;
+ssize_t array_locate(const void *data, const void *target,
+                     const size_t elem_size, const size_t elem_count) {
+  return 0;
 }
