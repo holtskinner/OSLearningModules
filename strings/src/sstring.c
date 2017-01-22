49d1479f1cff6578264a9b6be7629fbe7d82653b
Skinner <skinnerh@health.missouri.edu>
2017-01-21 20:13:07 -0600

    finished string length

--- strings/src/sstring.c
+++ strings/src/sstring.c
@@ -1,36 +1,56 @@
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../include/sstring.h"
 
 bool string_valid(const char *str, const size_t length) {
+  if (!str || length == 0 || str[length - 1] != '\0') {
+    return false;
+  }
 
-	return false;
+  return true;
 }
 
 char *string_duplicate(const char *str, const size_t length) {
+  if (!str || length == 0) {
+    return NULL;
+  }
 
-	return NULL;
+  char *duplicate = calloc(length, sizeof(char));
 
+  if (!strncpy(duplicate, str, length)) {
+    return NULL;
+  }
+
+  return duplicate;
 }
 
 bool string_equal(const char *str_a, const char *str_b, const size_t length) {
+  if (!str_a || !str_b || length == 0) {
+    return false;
+  }
 
-	return false;
+  if (strncmp(str_a, str_b, length) == 0) {
+    return true;
+  }
+  return false;
 }
 
 int string_length(const char *str, const size_t length) {
+  if (!str || length == 0) {
+    return -1;
+  }
 
-	return 0;
+  return strlen(str);
 }
 
-int string_tokenize(const char *str, const char *delims, const size_t str_length,char **tokens, const size_t max_token_length, const size_t requested_tokens) {
-
-	return 0;
+int string_tokenize(const char *str, const char *delims,
+                    const size_t str_length, char **tokens,
+                    const size_t max_token_length,
+                    const size_t requested_tokens) {
+  return 0;
 }
 
-bool string_to_int(const char *str, int *converted_value) {
-
-	return true;
-}
+bool string_to_int(const char *str, int *converted_value) { return true; }
