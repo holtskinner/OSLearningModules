3d8a0f94d1270d22c7255411617a864ebb3cf93b
Skinner <skinnerh@health.missouri.edu>
2017-01-22 16:24:55 -0600

    Got bitmap functions working up to bitmap_test

--- bits/src/bitmap.c
+++ bits/src/bitmap.c
@@ -1,40 +1,76 @@
 #include "../include/bitmap.h"
-
-// data is an array of uint8_t and needs to be allocated in bitmap_create
-//      and used in the remaining bitmap functions. You will use data for any bit operations and bit logic
+#include <math.h>
+// data is an array of uint8_t and needs to be allocated in bitmap_create and
+// used in the remaining bitmap functions. You will use data for any bit
+// operations and bit logic
 // bit_count the number of requested bits, set in bitmap_create from n_bits
 // byte_count the total number of bytes the data contains, set in bitmap_create
 
-
 bitmap_t *bitmap_create(size_t n_bits) {
-
+  if (n_bits <= 0) {
     return NULL;
+  }
+
+  bitmap_t *bitmap = calloc(n_bits, sizeof(bitmap_t));
+
+  if (!bitmap) {
+    free(bitmap);
+    bitmap = NULL;
+    return NULL;
+  }
+
+  bitmap->data = calloc(n_bits, sizeof(uint8_t));
+  bitmap->bit_count = n_bits;
+  bitmap->byte_count = ceil(n_bits / 8.0);  // convert bits to bytes
+
+  return bitmap;
 }
 
 bool bitmap_set(bitmap_t *const bitmap, const size_t bit) {
-
-	return false;
+  if (!bitmap || bit < 0 || bit > bitmap->bit_count) {
+    return false;
+  }
+  bitmap->data[bit / 8] |= 1 << (bit % 8);
+  return true;
 }
 
 bool bitmap_reset(bitmap_t *const bitmap, const size_t bit) {
-
-	return false;
+  if (!bitmap || bit < 0 || bit > bitmap->bit_count) {
+    return false;
+  }
+  bitmap->data[bit / 8] &= ~(1 << (bit % 8));
+  return true;
 }
 
 bool bitmap_test(const bitmap_t *const bitmap, const size_t bit) {
-	return false;
+  if (!bitmap || bit < 0 || bit > bitmap->bit_count) {
+    return false;
+  }
+
+  if (bitmap->data[bit / 8] & (1 << (bit % 8))) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-size_t bitmap_ffs(const bitmap_t *const bitmap) {
+size_t bitmap_ffs(const bitmap_t *const bitmap) { return SIZE_MAX; }
 
-	return 0;
-}
-
-size_t bitmap_ffz(const bitmap_t *const bitmap) {
-
-	return 0;
-}
+size_t bitmap_ffz(const bitmap_t *const bitmap) { return SIZE_MAX; }
 
 bool bitmap_destroy(bitmap_t *bitmap) {
-	return false;
+  if (!bitmap || !bitmap->data) {
+    return false;
+  }
+
+  free(bitmap->data);
+  bitmap->data = NULL;
+
+  free(bitmap);
+  bitmap = NULL;
+
+  if (bitmap) {
+    return false;
+  }
+  return true;
 }
