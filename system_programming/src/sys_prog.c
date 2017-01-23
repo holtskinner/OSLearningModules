be780d57328336451b5d2143e6e17d4f815016e4
Skinner <skinnerh@health.missouri.edu>
2017-01-23 17:03:18 -0600

    Almost there! ALl system programming done except endian converter

--- system_programming/src/sys_prog.c
+++ system_programming/src/sys_prog.c
@@ -10,24 +10,54 @@
 
 // LOOK INTO OPEN, READ, WRITE, CLOSE, FSTAT/STAT, LSEEK
 // GOOGLE FOR ENDIANESS HELP
+// NOTE THE FILE I/O MUST USE OPEN, READ, WRITE, CLOSE, SEEK, STAT with file
+// descriptors (NO FILE*)
+// Make sure to uint8_t or uint32_t, you are dealing with system dependent sizes
 
-bool bulk_read(const char *input_filename, void *dst, const size_t offset, const size_t dst_size) {
+bool bulk_read(const char *input_filename, void *dst, const size_t offset,
+               const size_t dst_size) {
+  if (!input_filename || !dst || offset < 0 || dst_size <= 0) {
+    return false;
+  }
 
-	return false;
+  int file = open(input_filename, O_RDONLY);
+
+  if (file < 0 || lseek(file, offset, SEEK_SET) < 0 ||
+      read(file, dst, dst_size) < 0) {
+    close(file);
+    return false;
+  }
+
+  close(file);
+  return true;
 }
 
-bool bulk_write(const void *src, const char *output_filename, const size_t offset, const size_t src_size) {
+bool bulk_write(const void *src, const char *output_filename,
+                const size_t offset, const size_t src_size) {
+  if (!src || !output_filename || offset < 0 || src_size <= 0) {
+    return false;
+  }
 
-	return false;
+  int file = open(output_filename, O_WRONLY);
+
+  if (file < 0 || lseek(file, offset, SEEK_SET) < 0 ||
+      write(file, src, src_size) < 0) {
+    close(file);
+    return false;
+  }
+
+  close(file);
+  return true;
 }
-
 
 bool file_stat(const char *query_filename, struct stat *metadata) {
-
-	return false;
+  if (stat(query_filename, metadata) < 0) {
+    return false;
+  }
+  return true;
 }
 
-bool endianess_converter(uint32_t *src_data, uint32_t *dst_data, const size_t src_count) {
-
-	return false;
+bool endianess_converter(uint32_t *src_data, uint32_t *dst_data,
+                         const size_t src_count) {
+  return false;
 }
