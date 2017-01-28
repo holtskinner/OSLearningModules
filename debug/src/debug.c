70165c8131257a1c3fdf8239129a4eebc943b4c6
Skinner <skinnerh@health.missouri.edu>
2017-01-27 20:06:25 -0600

    Removed unnessecary temp array in debug.c

--- debug/src/debug.c
+++ debug/src/debug.c
@@ -13,28 +13,16 @@ bool terrible_sort(uint16_t *data_array, const size_t value_count) {
   if (!data_array || value_count <= 0) {
     return false;
   }
-  // temp array
-  uint16_t *sorting_array = (uint16_t *)malloc(value_count * sizeof(uint16_t));
 
-  if (!sorting_array) {
-    return false;
-  }
+  // got rid of the temp array, not needed
 
-  size_t i = 0;
-  for (i = 0; i < value_count; ++i) {
-    sorting_array[i] = data_array[i];
-  }
   // where the magic happens
-  qsort(sorting_array, value_count, sizeof(uint16_t), comparator_func);
+  qsort(data_array, value_count, sizeof(uint16_t), comparator_func);
 
-  // check that the array is in increasing order
-  for (i = 1; i < value_count; ++i) {
-    if (sorting_array[i] < sorting_array[i - 1]) {
+  for (size_t i = 1; i < value_count; ++i) {
+    if (data_array[i] < data_array[i - 1]) {
       return false;
     }
   }
-  // copy temp array into original
-  memcpy(data_array, sorting_array, value_count * sizeof(uint16_t));
-  free(sorting_array);
   return true;
 }
