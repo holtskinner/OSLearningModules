e77f195f05e890f3f8bbd21d99405f4af710d27b
Skinner <skinnerh@health.missouri.edu>
2017-01-27 20:02:57 -0600

    fixed debug.c

--- debug/src/debug.c
+++ debug/src/debug.c
@@ -4,17 +4,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-// protected function, that only this .c can use
+// sort in increasing order
 int comparator_func(const void *a, const void *b) {
-  return (uint8_t *)a - (uint8_t *)b;
+  return *(uint8_t *)a - *(uint8_t *)b;
 }
 
 bool terrible_sort(uint16_t *data_array, const size_t value_count) {
   if (!data_array || value_count <= 0) {
     return false;
   }
+  // temp array
+  uint16_t *sorting_array = (uint16_t *)malloc(value_count * sizeof(uint16_t));
 
-  qsort(data_array, value_count, sizeof(uint16_t), comparator_func);
+  if (!sorting_array) {
+    return false;
+  }
 
+  size_t i = 0;
+  for (i = 0; i < value_count; ++i) {
+    sorting_array[i] = data_array[i];
+  }
+  // where the magic happens
+  qsort(sorting_array, value_count, sizeof(uint16_t), comparator_func);
+
+  // check that the array is in increasing order
+  for (i = 1; i < value_count; ++i) {
+    if (sorting_array[i] < sorting_array[i - 1]) {
+      return false;
+    }
+  }
+  // copy temp array into original
+  memcpy(data_array, sorting_array, value_count * sizeof(uint16_t));
+  free(sorting_array);
   return true;
 }
