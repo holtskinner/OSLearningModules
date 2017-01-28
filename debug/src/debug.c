1d9ce8e7e6dba198c8ec8aa055e3473160b41c01
Skinner <skinnerh@health.missouri.edu>
2017-01-27 18:52:36 -0600

    Fixed Create Blacnk Records

--- debug/src/debug.c
+++ debug/src/debug.c
@@ -1,30 +1,20 @@
 #include "../include/debug.h"
-
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 // protected function, that only this .c can use
 int comparator_func(const void *a, const void *b) {
-    return (uint8_t *)a - (uint8_t *)b;
+  return (uint8_t *)a - (uint8_t *)b;
 }
 
 bool terrible_sort(uint16_t *data_array, const size_t value_count) {
-    uint16_t sorting_array *= malloc(value_count * sizeof(*data_array));
-    for (int i = 0; i < value_count; ++i) {
-        sorting_array[i] = data_array[i];
-    }
+  if (!data_array || value_count <= 0) {
+    return false;
+  }
 
-    qsort(sorting_array, value_count, sizeof(sorting_array) / sizeof(uint16_t), comparator_func)
+  qsort(data_array, value_count, sizeof(uint16_t), comparator_func);
 
-    bool sorted = true;
-    for (int i = 0; i < value_count ++i) {
-        sorted &= sorting_array[i] < sorting_array[i + 1];
-    }
-
-    if (sorted) {
-        memcpy(sorting_array, *data_array, value_count);
-    }
-    return sorted;
+  return true;
 }
-
--- error_handling/src/error_handling.c
+++ error_handling/src/error_handling.c
@@ -1,39 +1,56 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <errno.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include "../include/error_handling.h"
 
 int create_blank_records(Record_t **records, const size_t num_records) {
+  if (!records || *records || num_records <= 0) {
+    return -1;
+  }
 
-	*records = (Record_t*) malloc(sizeof(Record_t) * num_records);
+  *records = (Record_t *)malloc(sizeof(Record_t) * num_records);
 
-	memset(*records,0,sizeof(Record_t) * num_records);
-	return 0;	
+  if (!(*records)) {
+    return -2;
+  }
+
+  memset(*records, 0, sizeof(Record_t) * num_records);
+
+  return 0;
 }
 
-int read_records(const char *input_filename, Record_t *records, const size_t num_records) {
-
-	int fd = open(input_filename, O_RDONLY);
+int read_records(const char *input_filename, Record_t *records,
+                 const size_t num_records) {
+  int fd = open(input_filename, O_RDONLY);
 
   ssize_t data_read = 0;
   for (size_t i = 0; i < num_records; ++i) {
-		data_read = read(fd,&records[i], sizeof(Record_t));	
-	}
-	return 0;
+    data_read = read(fd, &records[i], sizeof(Record_t));
+  }
+  return 0;
 }
 
-int create_record(Record_t **new_record, const char* name, int age) {
+int create_record(Record_t **new_record, const char *name, int age) {
+  if (!new_record || !name || age <= 0 || age > 200 || strlen(name) > 50) {
+    return (-1);
+  }
 
-	*new_record = (Record_t*) malloc(sizeof(Record_t));
-	
-	memcpy((*new_record)->name,name,sizeof(char) * strlen(name));
-	(*new_record)->name[MAX_NAME_LEN - 1] = 0;	
-	(*new_record)->age = age;
-	return 0;
+  *new_record = (Record_t *)calloc(1, sizeof(Record_t));
 
+  if (!(*new_record)) {
+    return -2;
+  }
+
+  if (!memcpy((*new_record)->name, name, sizeof(char) * strlen(name))) {
+    return -2;
+  }
+
+  (*new_record)->name[MAX_NAME_LEN - 1] = '\0';
+  (*new_record)->age = age;
+  return 0;
 }
